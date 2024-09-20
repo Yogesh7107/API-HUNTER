ee139b1091b9795b723ddc405a1d782b2b6e3b08
Yogesh Chaudhari <yogeshdc494@gmail.com>
2024-09-20 15:26:14 +0530

    updates

---
+++ src/Routes/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to the Product App</h1>
+      <nav>
+        <Link to="/login">
+          <h3 data-testid="login-link">Login Page</h3>
+        </Link>
+        <Link to="/dashboard">
+          <h3 data-testid="home-link">Home</h3> </Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Home;
